10004e76be6bf8802b1c0618cc70fec92d35fc22
Lakshay <englakshaysk@gmail.com>
2022-11-10 12:11:27 +0530

    updated recommendations page

--- components/Recommendations.js
+++ components/Recommendations.js
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { AiOutlineSearch } from 'react-icons/ai'
 import { MdMarkEmailUnread } from 'react-icons/md'
 import lakshay from '../public/lakshay.jpg'
+import jsLogo from '../public/jsLogo.png'
 
 const styles = {
     wrapper: 'h-screen min-w-[10rem] max-w-[30rem] flex-[1.2] p-[2rem]',
@@ -12,8 +13,11 @@ const styles = {
     authorProfileImageContainer: 'h-[5rem] w-[5rem] rounded-full overflow-hidden',
     authorName: 'font-semibold mb-[.2rem] mt-[1rem]',
     authorFollowing: 'text-[#787878]',
-    authorActions: '',
-    actionButton: '',
+    authorActions: 'flex gap-[.6rem] my-[1rem]',
+    actionButton: 'bg-[#1A8917] text-white rounded-full px-[.6rem] py-[.4rem] text-sm',
+    recAuthProfile: 'rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]',
+    recommendationAuthorName: 'text-sm',
+    recommendationAuthorContainer: 'flex items-center gap-[.6rem]'
 }
 
 const Recommendations = () =>{
@@ -40,7 +44,27 @@ const Recommendations = () =>{
             <div className={styles.authorFollowing}>1M followers</div>
             <div className={styles.authorActions}>
                 <button className={styles.actionButton}>Follow</button>
-                <button className={styles.actionButton}></button>
+                <button className={styles.actionButton}><MdMarkEmailUnread/></button>
+            </div>
+        </div>
+        <div className={styles.recommendationContainer}>
+            <div className={styles.title}>More from Medium</div>
+            <div className={styles.articleConatiner}>
+                <div className={styles.recommendationAuthorContainer}>
+                    <div className={styles.recAuthProfile}>
+                    <Image
+                        src={lakshay}
+                        height={100}
+                        width={100}
+                    /> 
+                </div>
+                    <div className={styles.recommendationAuthorName}>Lakshay</div>
+                </div>
+                    <Image
+                        src={jsLogo}
+                        height={100}
+                        width={100}
+                    />
             </div>
         </div>
     </div>
